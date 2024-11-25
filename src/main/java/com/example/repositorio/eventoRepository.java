215cd15c7a58d87301bc8bf84f7fac6a2c564e5f
celso <celso.san2101@alumnos.ubiobio.cl>
2024-11-25 16:30:43 -0300

    base para la creacion

--- src/main/java/com/example/repositorio/eventoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.repositorio;
-
-import com.example.model.evento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface eventoRepository extends JpaRepository<evento, Integer> {
-}
